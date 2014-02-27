e3d3f8e02adcac306a69ae43571d8f4fd9d67c25
Emil Johansson <emij@student.chalmers.se>
2014-02-27 21:39:51 +0100

    small change

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 
+import edu.cmu.sphinx.recognizer.Recognizer;
+
 public class Main {
 	public static void main(String[] args){
 		TestVoice tst = new TestVoice();
+	
 		tst.start();
 	}
 
