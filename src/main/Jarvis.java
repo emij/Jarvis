0717802bcdbe804349bba04af3d1165ccf1ffe54
Marika Hansson <hansson.marika@gmail.com>
2014-02-27 16:28:56 +0100

    Made skeleton for core class for recognition and input from source

---
+++ src/main/Jarvis.java
@@ -0,0 +1,14 @@
+/*
+ * Class for recognition and input from source
+ */
+
+package main;
+
+public class Jarvis {
+
+	//TODO: set inputed configuration manager or default from file
+	public void setConfiguration(){
+		
+	}
+	
+}
