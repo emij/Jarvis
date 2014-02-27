5925dc5a7893d81e4331c8fe38b703a09e4fcdb5
Marika Hansson <hansson.marika@gmail.com>
2014-02-27 16:58:18 +0100

    Started on code for setting configmanga

--- src/main/Jarvis.java
+++ src/main/Jarvis.java
@@ -4,11 +4,20 @@
 
 package main;
 
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+
 public class Jarvis {
 
+	ConfigurationManager cm;
+	
 	//TODO: set inputed configuration manager or default from file
-	public void setConfiguration(){
-		
+	public void setConfiguration(String s){
+		if(s.isEmpty()){
+			cm = new ConfigurationManager("Jarvis.config.xml");
+		}
+		else{
+			cm = new ConfigurationManager(s);
+		}
 	}
 	
 }
