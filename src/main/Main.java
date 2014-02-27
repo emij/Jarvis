8a3f3a5ff8ea46d3fd62b42e653427c9b4cdae84
Marika Hansson <hansson.marika@gmail.com>
2014-02-27 16:21:21 +0100

    Added skeleton for public method towards interface.

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,29 @@
+/*
+ * Layer class between recognition code and classes for communication with hardware.
+ * 
+ */
+
 package main;
 
 public class Main {
-
+	
+	//TODO: method to activate recognizing
+	public void Activate(){
+		
+	}
+	
+	//TODO: method to deactivate recognizing
+	public void Deactive(){
+		
+	}
+	
+	//TODO: method for disabling microphone
+	public void DisableMicrophone(){
+		
+	}
+	
+	//TODO: method for enabling microphone
+	public void EnableMicrophone(){
+		
+	}
 }
