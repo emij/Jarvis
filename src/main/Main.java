24c93fc5c86b6aab69d9e6597e995f3cd6836d9a
Emil Johansson <emij@student.chalmers.se>
2014-02-27 17:00:36 +0100

    Added classes for simple voice recognition. Working how to send results to main class

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,5 +1,9 @@
 package main;
 
 public class Main {
+	public static void main(String[] args){
+		TestVoice tst = new TestVoice();
+		tst.start();
+	}
 
 }
---
+++ src/main/TestVoice.java
@@ -0,0 +1,52 @@
+package main;
+
+import edu.cmu.sphinx.frontend.util.Microphone;
+import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
+import edu.cmu.sphinx.util.props.ConfigurationManager;
+
+
+public class TestVoice extends Thread {
+	private ConfigurationManager cm;
+	private Recognizer recognizer;
+	private Result result;
+	public TestVoice() {
+		cm = new ConfigurationManager(TestVoice.class.getResource("testVoice.config.xml"));
+
+		recognizer = (Recognizer) cm.lookup("recognizer");
+		recognizer.allocate();
+
+		// start the microphone or exit if the program if this is not possible
+		Microphone microphone = (Microphone) cm.lookup("microphone");
+		if (!microphone.startRecording()) {
+			System.out.println("Cannot start microphone.");
+			recognizer.deallocate();
+			System.exit(1);
+		}
+    }
+	
+
+    // loop the recognition until the program exits.
+	public void run() {
+		System.out.println("lamp ( On | Off ) or  radio ( On | Off)");
+		while (true) {
+			System.out.println("Start speaking. Press Ctrl-C to quit.\n");
+
+			result = recognizer.recognize();
+
+			if (result != null) {
+				String resultText = result.getBestFinalResultNoFiller();
+				if(resultText.length() != 0){
+					System.out.println("You said: " + resultText + '\n');
+				} else {
+					System.out.println("no text");
+				}
+			} else {
+				System.out.println("I can't hear what you said.\n");
+			}
+		}
+	}
+}
+
+
+	
