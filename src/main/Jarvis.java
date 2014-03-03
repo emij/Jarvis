48a0ddc0d39457c4a085f2f9c791b436196ead47
Marika <hmarika@student.chalmers.se>
2014-03-03 11:31:20 +0100

    Added constructors and created method for setup off mic and recognizer

--- src/main/Jarvis.java
+++ src/main/Jarvis.java
@@ -7,15 +7,30 @@ package main;
 import java.io.IOException;
 import java.net.URL;
 
+import edu.cmu.sphinx.frontend.util.Microphone;
+import edu.cmu.sphinx.recognizer.Recognizer;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 import edu.cmu.sphinx.util.props.PropertyException;
 
 public class Jarvis {
 
 	ConfigurationManager cm;
+	Recognizer recognizer;
+	Microphone microphone;
 	
-	public void setConfiguration(URL s){
-		if(s == null){
+
+	public Jarvis(URL u){
+		setConfiguration(u);
+		setup();
+	}
+	
+	public Jarvis(){
+		setConfiguration(null);
+		setup();
+	}
+	
+	public void setConfiguration(URL u){
+		if(u == null){
 			try {
 				cm = new ConfigurationManager(new URL("config.xml"));
 			} catch (IOException | PropertyException e) {
@@ -25,7 +40,7 @@ public class Jarvis {
 		}
 		else{
 			try {
-				cm = new ConfigurationManager(s);
+				cm = new ConfigurationManager(u);
 			} catch (IOException | PropertyException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
@@ -33,4 +48,14 @@ public class Jarvis {
 		}
 	}
 	
+	private void setup(){
+		try {
+		    recognizer = (Recognizer) cm.lookup("recognizer");
+		    microphone = (Microphone) cm.lookup("microphone");
+		}
+		catch (PropertyException | InstantiationException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
