eb9c316c7949a94eca49ef28e44e4ed2f56246f6
Marika <hmarika@student.chalmers.se>
2014-03-03 11:47:17 +0100

    Implemented run method for recordning from mic

--- src/main/Jarvis.java
+++ src/main/Jarvis.java
@@ -9,10 +9,11 @@ import java.net.URL;
 
 import edu.cmu.sphinx.frontend.util.Microphone;
 import edu.cmu.sphinx.recognizer.Recognizer;
+import edu.cmu.sphinx.result.Result;
 import edu.cmu.sphinx.util.props.ConfigurationManager;
 import edu.cmu.sphinx.util.props.PropertyException;
 
-public class Jarvis {
+public class Jarvis extends Thread{
 
 	ConfigurationManager cm;
 	Recognizer recognizer;
@@ -52,10 +53,35 @@ public class Jarvis {
 		try {
 		    recognizer = (Recognizer) cm.lookup("recognizer");
 		    microphone = (Microphone) cm.lookup("microphone");
+		   
+		    recognizer.allocate();
 		}
-		catch (PropertyException | InstantiationException e) {
+		catch (PropertyException | InstantiationException | IllegalStateException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}
+	}
+	
+	@Override
+	public void run(){
+		if(microphone.startRecording()){
+			while(true){
+				System.out.println("Speak command please");
+				
+				Result r = recognizer.recognize();
+				
+				if(r != null){
+					//TODO: notify new command
+				}
+				else{
+					System.out.println("Cannot hear command, please try again");
+				}
+			}
+		}
+		else{
+			System.out.println("Cannot start microphone");
+			recognizer.deallocate();
+			System.exit(1); //Error occurred
 		}
 	}
 }
