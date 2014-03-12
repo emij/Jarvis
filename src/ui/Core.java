ea76b9ba0721794722c6c93281107b7b7bdb6696
Emil Johansson <emij@student.chalmers.se>
2014-03-12 11:14:20 +0100

    Added mute/unmute functionality

--- src/ui/Core.java
+++ src/ui/Core.java
@@ -13,28 +13,29 @@ import voice.VoiceCommand;
 
 public class Core implements Observer  {
 	private VoiceCommand voiceCommand;
-	
+	private String mic = "microphone";
 	private Map<String, Device> devices = new HashMap<String, Device>(); 
-		
+
 	public Core(){
 		// Adding devices to hashmap
 		setUpDevices();
 
 		voiceCommand = new VoiceCommand(); 
 		voiceCommand.addObserver(this);
-		
+
 		TestVoice tst = new TestVoice(voiceCommand);
 		tst.start();
 	}
 	private void setUpDevices() {
 		// TODO Will do this in a better way down the road. possible load everything from a settings file
-			
+
 		addDevice(new Device("lamp"));
 		addDevice(new Device("kitchen"));
 		addDevice(new Device("coffee"));
 		addDevice(new Device("bathroom"));
 		addDevice(new Device("radio"));
-		
+		addDevice(new Device("microphone"));
+
 	}
 	public void addDevice(Device dev){
 		devices.put(dev.getName(), dev);
@@ -47,31 +48,35 @@ public class Core implements Observer  {
 	public void update(Observable o, Object arg) {
 		if (o instanceof VoiceCommand){
 			VoiceCommand vCommand = (VoiceCommand)o;
-			
+
 			// Divide voicecommand into a stringarray
 			String[] strArray = vCommand.getCommand().split(" ");
 			Device dev = devices.get(strArray[0]); // get device from hashmap
 			if(dev != null){
-				try {
-					Method method = dev.getClass().getDeclaredMethod(strArray[1], new Class[] {});
-					method.invoke(dev, null);
-					// TODO create proper exceptionhandling
-				} catch (IllegalArgumentException e) {
-				} catch (IllegalAccessException e) {
-				} catch (InvocationTargetException e) {
-				} catch (NoSuchMethodException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				} catch (SecurityException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+				if(devices.get(mic).isActive() || 
+						dev.getName().equalsIgnoreCase(mic)){
+					try {
+						Method method = dev.getClass().getDeclaredMethod(strArray[1], new Class[] {});
+						method.invoke(dev, null);
+						// TODO create proper handling of exceptions
+					} catch (IllegalArgumentException e) {
+					} catch (IllegalAccessException e) {
+					} catch (InvocationTargetException e) {
+					} catch (NoSuchMethodException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					} catch (SecurityException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
 				}
 			} else {
 				System.out.println("I am sorry, but the device " + strArray[0] + " is not installed");
-			}
-			
-		}
-		
-	}
+			} 
 
+		}
+
+	}
 }
+
+
