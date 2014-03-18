5fd3b91d4ca3243a7fd45eebd7a0fb511ff59716
Marika Hansson <hansson.marika@gmail.com>
2014-03-18 14:53:32 +0100

    class for creation of command object

---
+++ src/main/CommandObject.java
@@ -0,0 +1,31 @@
+package main;
+
+public class CommandObject {
+	
+	String action;
+	String device;
+	
+	public CommandObject() {
+	}
+	
+	public void addAction(String action){
+		this.action = action;
+	}
+	
+	public void addDevice(String device){
+		this.device = device;
+	}
+	
+	public void addPosition(String position){
+		
+	}
+	
+	public void addParam(String param){
+		
+	}
+	
+	public void generateCommand(){
+		Command c = new Command();
+		c.newCommand(action + " " + device);
+	}
+}
