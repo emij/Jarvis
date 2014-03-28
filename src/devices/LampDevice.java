77e813f829cd671673ae570a267ae8231224b2a5
Jacob Lundberg <jacoblu@ce-pc14.ed4211.grp.cse.a.1.hb.cdal.chalmers.se>
2014-03-28 11:40:42 +0100

    Added basic device control, able to control a lamp

--- src/devices/LampDevice.java
+++ src/devices/LampDevice.java
@@ -1,5 +1,7 @@
 package devices;
 
+import java.io.IOException;
+
 public class LampDevice extends AbstractDevice {
 
 	public LampDevice(String name) {
@@ -14,7 +16,7 @@ public class LampDevice extends AbstractDevice {
 	public boolean enable() {
 		if(!isActive()){
 			System.out.println(getName() + " turned on WOOHOOO");
-			flipSwitch();
+			flipSwitch(1);
 			return true;
 		} else {
 			return false;
@@ -25,11 +27,24 @@ public class LampDevice extends AbstractDevice {
 	public boolean disable() {
 		if(isActive()){
 			System.out.println(getName() + " turned off");
-			flipSwitch();
+			flipSwitch(0);
 			return true;
 		} else {
 		return false;
 		}
+	}
+	
+	//Funkar inte att ha det på detta sättet, om t.ex. tänding misslyckas måste man först säga lamp disable innan
+	//det kan gå att tända
+	public void flipSwitch(int state) {
+		String command = "pihat --repeats=50 --id=1 --channel=0 --state="+state;
+		try {
+			Process p = Runtime.getRuntime().exec(command);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace(); //Fixa exception
+		}
+		super.flipSwitch();
 	}
 
 }
