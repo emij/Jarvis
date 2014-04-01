18e942f22e05661e84649b00652d649c04aefd25
Emil Johansson <emij@student.chalmers.se>
2014-04-01 13:13:06 +0200

    Login screen is built. A lot of problems with aligning everything

--- src/client/LoginScreen.java
+++ src/client/LoginScreen.java
@@ -12,6 +12,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.ColumnConstraints;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
@@ -21,13 +22,13 @@ public class LoginScreen extends Application {
 	private GUIMediator mediator;
 	private Preferences prefs;
 	private String IP1,IP2,IP3,IP4, connectionPort, user; 
-	
+	private Insets inputBoxInsets = new Insets(3, 1, 3, 1);
 	public LoginScreen(GUIMediator mediator){
 		this.mediator = mediator;
 		prefs = Preferences.userRoot().node(this.getClass().getName());
 		setUpKeys();
 	}
-	
+
 	private void setUpKeys() {
 		IP1 = "IP1";
 		IP2 = "IP2";
@@ -35,7 +36,7 @@ public class LoginScreen extends Application {
 		IP4 = "IP4";
 		connectionPort = "connectionPort";
 		user = "user";
-		
+
 	}
 
 	public Preferences getPref(){
@@ -49,65 +50,90 @@ public class LoginScreen extends Application {
 		grid.setAlignment(Pos.CENTER);
 		grid.setHgap(10);
 		grid.setVgap(10);
+		GridPane leftGrid = new GridPane();
+		GridPane rightGrid = new GridPane();
+
+		ColumnConstraints column1 = new ColumnConstraints();
+		column1.setPercentWidth(50);
+		ColumnConstraints column2 = new ColumnConstraints();
+		column2.setPercentWidth(50);
+		grid.getColumnConstraints().addAll(column1, column2);
+
+		grid.add(leftGrid, 0, 0);
+		grid.add(rightGrid,1,0);
+
+		/*
 		Text scenetitle = new Text("Welcome");
 		scenetitle.setId("welcome-text");
-		grid.add(scenetitle, 0, 0, 2, 1);
+		grid.add(scenetitle, 0, 0, 5, 1);
+		 */
 
-		Label IP = new Label("IP:");
-		grid.add(IP, 0, 1);
+		// LeftGrid
 		
-		TextField IP1TextField = new TextField(prefs.get(IP1, "192"));
-		grid.add(IP1TextField, 1, 1);
-		TextField IP2TextField = new TextField(prefs.get(IP2, "168"));
-		grid.add(IP2TextField, 1, 1);
-		TextField IP3TextField = new TextField(prefs.get(IP3, "1"));
-		grid.add(IP3TextField, 1, 1);
-		TextField IP4TextField = new TextField(prefs.get(IP4, "1"));
-		grid.add(IP4TextField, 1, 1);
+		leftGrid.setPadding(new Insets(25, 25, 25, 25));
 
-		Label port = new Label("Port:");
-		grid.add(port, 0, 2);
-		
-		TextField portTextField = new TextField(prefs.get(connectionPort, "6789"));
-		grid.add(portTextField, 1, 2);
-
-		
 		Label userName = new Label("User Name:");
-		grid.add(userName, 0, 3);
-
-		TextField userTextField = new TextField(prefs.get(user, "jarvis"));
-		grid.add(userTextField, 1, 3);
+		leftGrid.add(userName, 0, 1);
 
 		Label pw = new Label("Password:");
-		grid.add(pw, 0, 4);
+		leftGrid.add(pw, 0, 2);
 
-		PasswordField pwBox = new PasswordField();
-		grid.add(pwBox, 1, 4);
-		grid.setPadding(new Insets(25, 25, 25, 25));
+		Label IP = new Label("IP:");
+		leftGrid.add(IP, 0, 3);
 
-		Button btn = new Button("Sign in");
-		HBox hbBtn = new HBox(10);
-		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-		hbBtn.getChildren().add(btn);
-		grid.add(hbBtn, 1, 5);
+		Label port = new Label("Port:");
+		leftGrid.add(port, 0, 4);
 		
 		final Text actiontarget = new Text();
-        grid.add(actiontarget, 1, 6);
-        
-        
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-        	 
-            @Override
-            public void handle(ActionEvent e) {
-            	actiontarget.setId("actiontarget");
-                actiontarget.setText("Sign in button pressed");
-            }
-        });
-        
+		leftGrid.add(actiontarget, 0, 5);
+
+		// RightGrid
+		rightGrid.setPadding(new Insets(25, 25, 25, 25));
+		
+		TextField userTextField = new TextField(prefs.get(user, "jarvis"));
+		rightGrid.add(userTextField, 0, 1,4,1);
+		GridPane.setMargin(userTextField, inputBoxInsets);
+
+		PasswordField pwBox = new PasswordField();
+		rightGrid.add(pwBox, 0, 2,4,1);
+		GridPane.setMargin(pwBox, inputBoxInsets);
+		
+		TextField IP1TextField = new TextField(prefs.get(IP1, "192"));
+		rightGrid.add(IP1TextField, 0, 3);
+		GridPane.setMargin(IP1TextField, inputBoxInsets);
+		TextField IP2TextField = new TextField(prefs.get(IP2, "168"));
+		rightGrid.add(IP2TextField, 1, 3);
+		GridPane.setMargin(IP2TextField, inputBoxInsets);
+		TextField IP3TextField = new TextField(prefs.get(IP3, "1"));
+		rightGrid.add(IP3TextField, 2, 3);
+		GridPane.setMargin(IP3TextField, inputBoxInsets);
+		TextField IP4TextField = new TextField(prefs.get(IP4, "1"));
+		rightGrid.add(IP4TextField, 3, 3);
+		GridPane.setMargin(IP4TextField, inputBoxInsets);
+		
+		TextField portTextField = new TextField(prefs.get(connectionPort, "6789"));
+		rightGrid.add(portTextField, 0, 4,4,1);
+		GridPane.setMargin(portTextField, new Insets(3, 0, 25, 0));
+
+		Button connectButton = new Button("Connect");
+		HBox hbBtn = new HBox(10);
+		hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
+		hbBtn.getChildren().add(connectButton);
+		rightGrid.add(hbBtn, 2, 5,2,1);
+
+		connectButton.setOnAction(new EventHandler<ActionEvent>() {
+
+			@Override
+			public void handle(ActionEvent e) {
+				actiontarget.setId("actiontarget");
+				actiontarget.setText("Sign in button pressed");
+			}
+		});
+
 		Scene scene = new Scene(grid, 500, 475);
 		primaryStage.setScene(scene);
 		scene.getStylesheets().add
-		 (LoginScreen.class.getResource("login.css").toExternalForm());
+		(LoginScreen.class.getResource("login.css").toExternalForm());
 		primaryStage.show();
 	}
 
